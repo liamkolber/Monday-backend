7e34fa1cc783b7e16fe5ac2b07e1340cefee84bd
Liam <liamkolber62@gmail.com>
2024-07-05 10:53:53 -0400

    Add update and delete (also test framework if I get to it)

---
+++ src/test/kotlin/example/com/collection/FragranceCollectionTest.kt
@@ -0,0 +1,12 @@
+package example.com.collection
+
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.shouldBe
+
+class FragranceCollectionTest : FunSpec({
+
+    test("should return list of all products") {
+       1 + 2 shouldBe 3
+    }
+ 
+ })
